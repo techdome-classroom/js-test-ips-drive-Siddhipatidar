65ee78aea5efb821d9fc5e046744a83d4c087b34
Siddhipatidar <128923358+Siddhipatidar@users.noreply.github.com>
2024-04-26 13:20:56 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,21 @@
-function longestSubstring(s) {
-
-
-    // Implementation of longestSubstring function
-}
-
-module.exports = { longestSubstring };
+function smallestMissingPositiveInteger(nums) {
+    // Handle the base case where the list is empty
+    if (!nums.length) return 1;
+  
+     const positiveNumbers = new Set();
+    for (const num of nums) {
+      if (num > 0) {
+        positiveNumbers.add(num);
+      }
+    }
+  
+    for (let i = 1; i <= positiveNumbers.size + 1; i++) {
+      if (!positiveNumbers.has(i)) {
+        return i;
+      }
+    }
+  
+    return positiveNumbers.size + 1;
+  }
+    
+    module.exports = smallestMissingPositiveInteger;
