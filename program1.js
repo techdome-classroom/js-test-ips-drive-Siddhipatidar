74570f7442af1599e33d74b3a26fea9a44e4e45e
Siddhipatidar <128923358+Siddhipatidar@users.noreply.github.com>
2024-04-26 13:19:46 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,25 @@
-function smallestMissingPositiveInteger(nums) {
+function longestSubstring(s) {
+  // Handle empty string case
+  if (!s) return 0;
 
+  let longest = 0;
+  let left = 0;
+  const charSet = new Set();
 
-    // Implement the function smallest_missing_positive_integer
-  
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+
+    while (charSet.has(currentChar)) {
+      charSet.delete(s[left]);
+      left++;
+    }
+
+    charSet.add(currentChar);
+
+    longest = Math.max(longest, right - left + 1);
   }
-  
-  module.exports = smallestMissingPositiveInteger;
+
+  return longest;
+}
+module.exports = { longestSubstring };
 
